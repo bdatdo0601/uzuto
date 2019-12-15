ec605a70d9079b14f26401f8469c943fdfb8879b
Dat Do <dat.b.do@gmail.com>
2019-12-15 14:16:25 -0500

    feat: Guest management

---
+++ src/components/Admin/Forms/GuestForm/index.js
@@ -0,0 +1,94 @@
+import React, { useState, useCallback } from "react";
+import PropTypes from "prop-types";
+import uuid from "uuid";
+import { Form, Modal, Button, Input, Icon, notification } from "antd";
+import pDebounce from "p-debounce";
+
+const formItemLayout = {
+    labelCol: {
+        xs: { span: 24 },
+        sm: { span: 8 },
+    },
+    wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 },
+    },
+};
+
+function GuestForm({ visible, form, onClose, onOk }) {
+    const [guestFields, setGuestField] = useState([]);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const onSubmit = useCallback(
+        pDebounce(async () => {
+            setIsSubmitting(true);
+            try {
+                const values = form.getFieldsValue();
+                await onOk(Object.values(values));
+                notification.success({
+                    message: "Guests are verified",
+                    description: "Guests will submit more information on their own",
+                });
+            } catch (err) {
+                console.error(err);
+                notification.error({
+                    message: "Something wrong! blame Dat",
+                    description: err.message,
+                });
+            }
+            setIsSubmitting(false);
+        }, 1000),
+        []
+    );
+    return (
+        <Form>
+            <Modal
+                title="Invite New Guest(s)"
+                visible={visible}
+                onCancel={onClose}
+                onOk={onSubmit}
+                okText="Invite"
+                okButtonProps={{ disabled: guestFields.length === 0 || isSubmitting, loading: isSubmitting }}
+            >
+                {guestFields.map((guest, index) => (
+                    <Form.Item key={guest} label={`Guest Email #${index + 1}`} {...formItemLayout}>
+                        {form.getFieldDecorator(guest, {
+                            rules: [
+                                {
+                                    type: "email",
+                                    message: "The input is not valid E-mail!",
+                                },
+                            ],
+                        })(<Input style={{ width: "60%", marginRight: 8 }} type="email" disabled={isSubmitting} />)}
+                        <Icon
+                            className="dynamic-delete-button"
+                            type="minus-circle-o"
+                            onClick={() => setGuestField(guestFields.filter(item => guest !== item))}
+                        />
+                    </Form.Item>
+                ))}
+                <Button
+                    type="dashed"
+                    disabled={isSubmitting}
+                    onClick={() => setGuestField([...guestFields, uuid()])}
+                    style={{ width: "100%" }}
+                >
+                    Add More Guest
+                </Button>
+            </Modal>
+        </Form>
+    );
+}
+
+GuestForm.propTypes = {
+    form: PropTypes.object.isRequired,
+    visible: PropTypes.bool.isRequired,
+    onClose: PropTypes.func,
+    onOk: PropTypes.func,
+};
+
+GuestForm.defaultProps = {
+    onClose: () => {},
+    onOk: () => {},
+};
+
+export default Form.create({ name: "Guest Form" })(GuestForm);
--- src/containers/Admin/GuestManagement/index.js
+++ src/containers/Admin/GuestManagement/index.js
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import LinhButton from "../../../components/LinhButton";
+import { Table } from "antd";
+import GuestForm from "../../../components/Admin/Forms/GuestForm";
+
+const columns = [
+    {
+        title: "Name",
+        dataIndex: "name",
+        key: "name",
+        fixed: "left",
+    },
+    {
+        title: "Email",
+        dataIndex: "email",
+        key: "email",
+    },
+    {
+        title: "Phone Number",
+        dataIndex: "phoneNumber",
+        key: "phoneNumber",
+    },
+    {
+        title: "Status",
+        dataIndex: "status",
+        key: "status",
+    },
+    {
+        title: "Company Count",
+        dataIndex: "companies",
+        key: "companies",
+    },
+    {
+        title: "Event Count",
+        dataIndex: "attendingEvents",
+        key: "attendingEvents",
+    },
+    {
+        title: "Resting Location",
+        dataIndex: "restLocation",
+        key: "restLocation",
+    },
+];
 
 export default function GuestManagement() {
+    const [modalVisible, setModalVisible] = useState(false);
     return (
-        <div className="text-center">
-            <h1 className="text-4xl my-8">Guest Management</h1>
-        </div>
+        <>
+            <GuestForm visible={modalVisible} onClose={() => setModalVisible(false)} />
+            <div className="text-center">
+                <h1 className="text-4xl my-8">Guest Management</h1>
+                <div className="flex-auto justify-end space-between my-8 w-full align-right text-right px-8">
+                    <LinhButton onClick={() => setModalVisible(true)}>Add New Guest</LinhButton>
+                </div>
+                <Table columns={columns} />
+            </div>
+        </>
     );
 }
