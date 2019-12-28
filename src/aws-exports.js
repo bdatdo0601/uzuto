f34e4e1f12311458e560965b854b90e1132fbf61
Dat Do <dat.b.do@gmail.com>
2019-12-27 21:09:51 -0500

    fix

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -4,7 +4,7 @@ const awsmobile = {
     "aws_project_region": "us-east-1",
     "aws_content_delivery_bucket": "uzuto-20191117155723-hostingbucket-devdd",
     "aws_content_delivery_bucket_region": "us-east-1",
-    "aws_content_delivery_url": "http://uzuto-20191117155723-hostingbucket-devdd.s3-website-us-east-1.amazonaws.com",
+    "aws_content_delivery_url": "https://d1psx3fwg1hbwe.cloudfront.net",
     "aws_cognito_identity_pool_id": "us-east-1:b2fd03e3-6d00-4ede-9ed9-e61567cc7b89",
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_66YstZUQa",
